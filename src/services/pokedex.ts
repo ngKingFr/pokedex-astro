3753246e257273085e02a112abbd718f02f7c1ec
choucry13 <beroucry@yahoo.fr>
2022-11-13 14:43:54 +0100

    fix(route): modify type parameter detail pokemon

--- src/services/pokedex.ts
+++ src/services/pokedex.ts
@@ -3,14 +3,12 @@ import type {Pokemon} from "../models/pokemon";
 export const getAllPokemon: () => Promise<Pokemon[]> = async () => {
     return await fetch('https://raw.githubusercontent.com/ruizalexandre/pokedex/master/pokedex.json').then(
         (response) => response.json()
-    ).then((result) => {
-        return result as Pokemon[];
-    })
+    ).then((result) => result as Pokemon[]);
 }
 
 
 export const getPokemonById: (id: number) => Promise<Pokemon> = async (id: number) => {
     return await getAllPokemon().then((pokemons) => {
         return pokemons[pokemons.findIndex(p => p.id === id)]
-    })
+    });
 }
